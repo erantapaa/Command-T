3335b5fd2efb56a0fa0c19dd7ec93a48d1068de5
Your Name <erantapaa@gmail.com>
2011-08-26 23:02:06 -0500

    Change matching algorithm to the 'wildcard spaces' version.

--- ruby/command-t/match.c
+++ ruby/command-t/match.c
@@ -21,6 +21,7 @@
 // ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 // POSSIBILITY OF SUCH DAMAGE.
 
+#include <ctype.h>
 #include "match.h"
 #include "ext.h"
 #include "ruby_compat.h"
@@ -44,86 +45,44 @@ double recursive_match(matchinfo_t *m,  // sharable meta-data
                        long last_idx,   // location of last matched character
                        double score)    // cumulative score so far
 {
-    double seen_score = 0;      // remember best score seen via recursion
-    int dot_file_match = 0;     // true if abbrev matches a dot-file
-    int dot_search = 0;         // true if searching for a dot
+  long i = abbrev_idx;
+  long j = str_idx;
 
-    for (long i = abbrev_idx; i < m->abbrev_len; i++)
-    {
-        char c = m->abbrev_p[i];
-        if (c == '.')
-            dot_search = 1;
-        int found = 0;
-        for (long j = str_idx; j < m->str_len; j++, str_idx++)
-        {
-            char d = m->str_p[j];
-            if (d == '.')
-            {
-                if (j == 0 || m->str_p[j - 1] == '/')
-                {
-                    m->dot_file = 1;        // this is a dot-file
-                    if (dot_search)         // and we are searching for a dot
-                        dot_file_match = 1; // so this must be a match
-                }
-            }
-            else if (d >= 'A' && d <= 'Z')
-                d += 'a' - 'A'; // add 32 to downcase
-            if (c == d)
-            {
-                found = 1;
-                dot_search = 0;
+  if (i == 0 && score == 0.0) {
+    goto match_space;
+  }
 
-                // calculate score
-                double score_for_char = m->max_score_per_char;
-                long distance = j - last_idx;
-                if (distance > 1)
-                {
-                    double factor = 1.0;
-                    char last = m->str_p[j - 1];
-                    char curr = m->str_p[j]; // case matters, so get again
-                    if (last == '/')
-                        factor = 0.9;
-                    else if (last == '-' ||
-                            last == '_' ||
-                            last == ' ' ||
-                            (last >= '0' && last <= '9'))
-                        factor = 0.8;
-                    else if (last >= 'a' && last <= 'z' &&
-                            curr >= 'A' && curr <= 'Z')
-                        factor = 0.8;
-                    else if (last == '.')
-                        factor = 0.7;
-                    else
-                        // if no "special" chars behind char, factor diminishes
-                        // as distance from last matched char increases
-                        factor = (1.0 / distance) * 0.75;
-                    score_for_char *= factor;
-                }
+  char a = m->abbrev_p[i];
+  char c = m->str_p[j];
 
-                if (++j < m->str_len)
-                {
-                    // bump cursor one char to the right and
-                    // use recursion to try and find a better match
-                    double sub_score = recursive_match(m, j, i, last_idx, score);
-                    if (sub_score > seen_score)
-                        seen_score = sub_score;
-                }
-
-                score += score_for_char;
-                last_idx = str_idx++;
-                break;
-            }
+  while (1) {
+    if (!a) break;
+    if (!c) break;
+    // test if c matches a
+    if (a == ' ') {
+match_space:
+      while (m->abbrev_p[i] == ' ') { i++; }
+      for ( ; j < m->str_len; j++) {
+        double s = recursive_match(m, j, i, 0, 1.0);
+        if (s > 0) {
+          return 1.0;
         }
-        if (!found)
-            return 0.0;
+      }
+      return 0.0;
     }
-    if (m->dot_file)
-    {
-        if (m->never_show_dot_files ||
-            (!dot_file_match && !m->always_show_dot_files))
-            return 0.0;
+    if (tolower(a) == tolower(c)) {
+      a = m->abbrev_p[++i];
+      c = m->str_p[++j];
+    } else {
+      return 0.0;
     }
-    return (score > seen_score) ? score : seen_score;
+  }
+
+  // either a != c or a or c is at end of string
+  if (!a) {
+    return 1.0; // reached end of pattern - return success
+  }
+  return 0.0;
 }
 
 // Match.new abbrev, string, options = {}
